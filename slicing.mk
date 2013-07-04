9c6f3a4c0f1ffb7194c86383d0c452cfae9fa28e
macmanes <macmanes@gmail.com>
2013-07-04 06:29:21 -0700

    629

--- slicing.mk
+++ slicing.mk
@@ -97,13 +97,13 @@ assemble:
 			$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 \
 			--seqType fq --JM 10G --min_contig_length $$MIN \
 			--left ~/trinityrnaseq-code/trunk/sample_data/test_Trinity_Assembly/reads.left.fq \
-			--right ~/trinityrnaseq-code/trunk/sample_data/test_Trinity_Assembly/reads.right.fq --CPU $(CPU) --output $$SLICE; fi; done \
+			--right ~/trinityrnaseq-code/trunk/sample_data/test_Trinity_Assembly/reads.right.fq --CPU $(CPU) --output $$SLICE; fi; done;
 		if [ $$SLICE -eq 20 ]; then \
 			MIN=1292 ; \
 			$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 \
 			--seqType fq --JM 10G --min_contig_length $$MIN \
 			--left ~/trinityrnaseq-code/trunk/sample_data/test_Trinity_Assembly/reads.left.fq \
-			--right ~/trinityrnaseq-code/trunk/sample_data/test_Trinity_Assembly/reads.right.fq --CPU $(CPU) --output $$SLICE; fi; done;
+			--right ~/trinityrnaseq-code/trunk/sample_data/test_Trinity_Assembly/reads.right.fq --CPU $(CPU) --output $$SLICE; fi; done
 
 rsem: $(SLICE).Trinity.fasta
 	$(TRINITY)/util/RSEM_util/run_RSEM_align_n_estimate.pl --transcripts $< --seqType fq --left $(READ1) \
