3c7fa3dc02b77304d77a2261cad30742167e0a52
macmanes <macmanes@gmail.com>
2013-07-03 17:53:02 -0700

    1752

--- slicing.mk
+++ slicing.mk
@@ -97,21 +97,13 @@ assemble:
 			$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 \
 			--seqType fa --JM 10G --min_contig_length $$MIN \
 			--left left.$$SLICE.rept.corr.fa \
-			--right right.$$SLICE.rept.corr.fa --CPU $(CPU) --output $$SLICE; fi; done
+			--right right.$$SLICE.rept.corr.fa --CPU $(CPU) --output $$SLICE; fi; done;
 		if [ $$SLICE -eq 20 ]; then \
 			MIN=1292 ; \
 			$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 \
 			--seqType fa --JM 10G --min_contig_length $$MIN \
 			--left left.$$SLICE.rept.corr.fa \
 			--right right.$$SLICE.rept.corr.fa --CPU $(CPU) --output $$SLICE; fi; done
-		if [ $$SLICE -eq 20 ]; then \
-			MIN=2221 ; \
-			$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 \
-			--seqType fa --JM 10G --min_contig_length $$MIN \
-			--left left.$$SLICE.rept.corr.fa \
-			--right right.$$SLICE.rept.corr.fa --CPU $(CPU) --output $$SLICE; fi; done
-
-
 
 rsem: $(SLICE).Trinity.fasta
 	$(TRINITY)/util/RSEM_util/run_RSEM_align_n_estimate.pl --transcripts $< --seqType fq --left $(READ1) \
