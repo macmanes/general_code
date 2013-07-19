ee15270abbfca7ae1c3666777525cadde851af53
Matthew MacManes <macmanes@gmail.com>
2013-07-19 12:14:31 -0700

    update assembly script

--- assembly.mk
+++ assembly.mk
@@ -23,14 +23,15 @@ CONFIG:= /home/macmanes/Dropbox/config.analy
 
 
 
-
+TRIM=5
 CPU=2
 RUN=run
 READ1=left.fastq
 READ2=right.fastq
 TRIMMOMATIC ?= $(shell which 'trimmomatic-0.30.jar')
 
-all: check trim correct merge assemble rsem
+.PHONY: check rsem
+all: check rsem
 
 check:
 	@echo "\n\n\n"###I am checking to see if you have all the dependancies installed.### "\n"
@@ -44,42 +45,40 @@ check:
 	@echo Reptile is installed"\n"
 	if [ -f $(READ1) ]; then echo 'left fastQ exists'; else echo 'Im having trouble finding your left fastQ file, check PATH \n'; exit 1; fi;
 	if [ -f $(READ2) ]; then echo 'right fastQ exists \n'; else echo 'Im having trouble finding your right fastQ file, check PATH \n'; exit 1; fi;
-trim: $(READ1) $(READ2)
+$(RUN)_left.$(TRIM).fastq $(RUN)_right.$(TRIM).fastq: $(READ1) $(READ2)
 	@echo About to start trimming
-	for TRIM in 10; do \
 		java -Xmx30g -jar $(TRIMMOMATIC) PE -phred33 -threads $(CPU) \
 		$(READ1) \
 		$(READ2) \
-		T.$$TRIM.pp.1.fq \
-		T.$$TRIM.up.1.fq \
-		T.$$TRIM.pp.2.fq \
-		T.$$TRIM.up.2.fq \
+		T.$(TRIM).pp.1.fq \
+		T.$(TRIM).up.1.fq \
+		T.$(TRIM).pp.2.fq \
+		T.$(TRIM).up.2.fq \
 		ILLUMINACLIP:$(BCODES):2:40:15 \
-		LEADING:$$TRIM TRAILING:$$TRIM SLIDINGWINDOW:4:$$TRIM MINLEN:25 ; \
-		cat T.$$TRIM.pp.1.fq T.$$TRIM.up.1.fq > $(RUN)_left.$$TRIM.fastq ; \
-		cat T.$$TRIM.pp.2.fq T.$$TRIM.up.2.fq > $(RUN)_right.$$TRIM.fastq ; \
-		rm T.$$TRIM.pp.2.fq T.$$TRIM.up.2.fq T.$$TRIM.pp.1.fq T.$$TRIM.up.1.fq ; \
-	done
+		LEADING:$(TRIM) TRAILING:$(TRIM) SLIDINGWINDOW:4:$(TRIM) MINLEN:25 ; 
+		cat T.$(TRIM).pp.1.fq T.$(TRIM).up.1.fq > $(RUN)_left.$(TRIM).fastq ; 
+		cat T.$(TRIM).pp.2.fq T.$(TRIM).up.2.fq > $(RUN)_right.$(TRIM).fastq ; 
+		rm T.$(TRIM).pp.2.fq T.$(TRIM).up.2.fq T.$(TRIM).pp.1.fq T.$(TRIM).up.1.fq ; 
 
-correct:
+both.fa both.q both.reptile.err: $(RUN)_left.$(TRIM).fastq $(RUN)_right.$(TRIM).fastq
 	@echo About to start error correction
 	fastq-converter-v2.0.pl ./ ./ 1 #files MUST have fastq extension
-	sed -i 's_[0-9]$_&/1_' $(RUN)_left.fa #add /1 to ID reads as left
-	sed -i 's_[0-9]$_&/2_' $(RUN)_right.fa #add /2 to ID reads as right
-	sed -i 's_^>.*[0-9]$_&/1_' $(RUN)_left.q
-	sed -i 's_^>.*[0-9]$_&/2_' $(RUN)_right.q
+	sed -i 's_[0-9]$$_&/1_' $(RUN)_left.fa #add /1 to ID reads as left
+	sed -i 's_[0-9]$$_&/2_' $(RUN)_right.fa #add /2 to ID reads as right
+	sed -i 's_^>.*[0-9]$$_&/1_' $(RUN)_left.q
+	sed -i 's_^>.*[0-9]$$_&/2_' $(RUN)_right.q
 	cat $(RUN)_left.fa $(RUN)_right.fa > both.fa
 	cat $(RUN)_left.q $(RUN)_right.q > both.q
 	reptile-omp $(CONFIG) #Do error corection
 
-merge: both.reptile.err
+$(RUN).left.rept.corr.fa $(RUN).right.rept.corr.fa: both.reptile.err
 	reptile_merger both.fa $< both.reptile.corr.fa #make error corrected fasta file
 	grep -aA1 '/1' both.reptile.corr.fa > $(RUN).left.rept.corr.fa
 	grep -aA1 '/2' both.reptile.corr.fa > $(RUN).right.rept.corr.fa
 	
 
-assemble:  $(RUN).left.rept.corr.fa $(RUN).right.rept.corr.fa
-	$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 --seqType fa --JM 30G \
+$(RUN).Trinity.fasta:  $(RUN).left.rept.corr.fa $(RUN).right.rept.corr.fa
+	$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 --seqType fa --JM 10G \
 	--left $(RUN).left.rept.corr.fa --right $(RUN).right.rept.corr.fa --group_pairs_distance 999 --CPU $(CPU) --output $(RUN)
 	
 rsem: $(RUN).Trinity.fasta
