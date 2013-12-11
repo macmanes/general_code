ff13f45478c5cfa9ff1a625aaf2ed1792d1a07bd
Matt MacManes <macmanes@gmail.com>
2013-12-11 11:15:33 -0500

    SE

--- new_assembly.mk
+++ new_assembly.mk
@@ -6,7 +6,7 @@
 ###
 ###        -limitations=  must use PE files.. no support for SE...
 ###
-###         -Make sure your Trinity is installed and in 
+###         -Make sure your BWA and Trinity are installed and in 
 ###          your path
 ###        
 ###          
@@ -35,16 +35,18 @@ all: check $(RUN)_left.$(TRIM).fastq $(RUN)_right.$(TRIM).fastq $(RUN).Trinity.f
 trim: check $(RUN)_left.$(TRIM).fastq $(RUN)_right.$(TRIM).fastq
 assemble: check $(RUN).Trinity.fasta
 express: check $(RUN).xprs
+single: check $(RUN)_SE.$(TRIM).fastq $(RUN).SE.Trinity.fasta 
+
 
 check:
 	@echo "\n\n\n"###I am checking to see if you have all the dependancies installed.### "\n"
 	command -v trimmomatic-0.32.jar >/dev/null 2>&1 || { echo >&2 "I require Trimmomatic but it's not installed.  Aborting."; exit 1; }
 	command -v bwa mem >/dev/null 2>&1 || { echo >&2 "I require BWA but it's not installed.  Aborting."; exit 1; }
-	@echo Trimmomatic is Installed
+	@echo BWA is Installed
 	command -v $(TRINITY) >/dev/null 2>&1 || { echo >&2 "I require Trinity but it's not installed.  Aborting."; exit 1; }
 	@echo Trinity is Installed
 	if [ -f $(READ1) ]; then echo 'left fastQ exists'; else echo 'Im having trouble finding your left fastQ file, check PATH \n'; exit 1; fi;
-	if [ -f $(READ2) ]; then echo 'right fastQ exists \n'; else echo 'Im having trouble finding your right fastQ file, check PATH \n'; exit 1; fi;
+	if [ -f $(READ2) ]; then echo 'right fastQ exists \n'; else echo 'Im having trouble finding your right fastQ file, check PATH \n'; fi;
 	chmod -w $(READ1)
 	chmod -w $(READ2)
 
@@ -74,6 +76,63 @@ $(RUN).xprs: $(RUN).Trinity.fasta
 		@echo --eXpress---
 		express -o $(RUN).xprs \
 		-p $(CPU) $(RUN).Trinity.fasta $(RUN).bam 2>express.log
+
+###SE Support###
+
+$(RUN)_SE.$(TRIM).fastq:$(READ1)
+	@echo About to start trimming
+		java -Xmx$(MEM)g -jar ${MAKEDIR}/trimmomatic-0.32.jar PE -phred$(PHRED) -threads $(CPU) \
+		$(READ1) \
+		$(RUN)_SE.$(TRIM).fastq \
+		ILLUMINACLIP:${MAKEDIR}/barcodes.fa:2:40:15 \
+		LEADING:$(TRIM) TRAILING:$(TRIM) SLIDINGWINDOW:4:$(TRIM) MINLEN:$(MINLEN) 2> trim.log
+
+$(RUN).SE.Trinity.fasta:$(RUN)_SE.$(TRIM).fastq
+	$(TRINITY) --full_cleanup --min_kmer_cov $(MINK) --seqType $(SEQ) --JM $(MEM)G --PasaFly --bflyHeapSpaceMax $(MEM)G --bflyCPU $(BCPU) \
+	--single $(RUN)_SE.$(TRIM).fastq --CPU $(CPU) --output $(RUN).SE
+
+$(RUN).SE.xprs:$(RUN).SE.Trinity.fasta
+		@echo ---Quantitiating Transcripts---
+		bwa index -p index $(RUN).SE.Trinity.fasta
+		bwa mem -t $(CPU) index $(READ1) 2>bwa.log | samtools view -Sb - > $(RUN).bam
+		samtools flagstat $(RUN).bam > $(RUN).map.stats &
+		@echo --eXpress---
+		express -o $(RUN).xprs \
+		-p $(CPU) $(RUN).SE.Trinity.fasta $(RUN).bam 2>express.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 nuclear: 
 	rm index* run.map.stats run.bam *log
 	rm -fr $(RUN)*
